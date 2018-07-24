764850525d61c6b48c2ce1a1010e8bbdf93a9681
Márcio Gabriel <marcioggs@gmail.com>
2018-07-24 20:51:01 -0300

    Changing text on added DOM nodes.

--- scripts/change-text.js
+++ scripts/change-text.js
@@ -7,7 +7,7 @@ String.prototype.replaceAll = function(search, replacement) {
 
 /**
  * Replace text.
- * @param text Text to be replaced
+ * @param {String} text Text to be replaced
  */
 function replaceText(text) {
     //TODO: Replace with words from options.html.
@@ -22,27 +22,12 @@ document.body.innerHTML = replaceText(document.body.innerHTML)
  * @param text Text to inspect
  */
 function hasTextToChange(text) {
-    return text.indexOf("Google") >= 0;
+    return typeof text == 'string' && text.indexOf("Google") >= 0;
 }
-
-/*
-const observer = new MutationObserver(function(mutations) {
-    console.log('passou 2');
-    document.body.innerHTML = document.body.innerHTML.replaceAll('Google', 'Microsoft');
-});
-observer.observe(document.body, { subtree: true, childList: true, characterData: true});
-
-const observer2 = new MutationObserver(function(mutations) {
-    //TODO: Filtrar apenas input
-    console.log('passou 3');
-    document.body.innerHTML = document.body.innerHTML.replaceAll('Google', 'Microsoft');
-});
-observer2.observe(document.body, { subtree: true,  childList: true, attributes: true, attributeFilter:  ["value"]});
-*/
 
 /**
  * Start observing object for relevant changes.
- * @param observer Object to be observed
+ * @param {MutationObserver} observer Observer
  */
 function startObserving(observer) {
     observer.observe(document.body, { subtree: true, childList: true, characterData: true, attributes: true, attributeFilter:  ["value"]});
@@ -50,26 +35,44 @@ function startObserving(observer) {
 
 /**
  * Replaces text on input value attribute, that can be read on the page.
- * @param mutation Mutated node
+ * @param {Node} node Node to search text to change
  */
-function replaceTextInInputValue(mutation) {
-    if (mutation.target.nodeName.toUpperCase() == 'INPUT') {
-        let text = mutation.target.getAttribute('value');
+function replaceTextInInputValue(node) {
+    if (node.nodeName.toUpperCase() == 'INPUT') {
+        let text = node.getAttribute('value');
         if (hasTextToChange(text)) {
-            mutation.target.setAttribute('value', replaceText(text));
+            node.setAttribute('value', replaceText(text));
         }
    }
 }
 
 /**
  * Replaces text in common text on page.
- * @param mutation Mutated node
+ * @param {Node} node Node to search text to change
  */
-function replaceTextInCharacterData(mutation) {
-    let data = mutation.target.data;
+function replaceTextInCharacterData(node) {
+    let data = node.data;
     if (hasTextToChange(data)) {
-        mutation.target.replaceData(0, data.length, replaceText(data));
+        node.replaceData(0, data.length, replaceText(data));
     }
+}
+
+/**
+ * Replaces text on added nodes and on their children recursively.
+ * @param {NodeList} nodes Nodes
+ */
+function replaceTextOnNodes(nodes) {
+    nodes.forEach((node) => {
+        replaceTextInCharacterData(node);
+        replaceTextInInputValue(node);
+        node.childNodes.forEach((childNode) => {
+            replaceTextInCharacterData(childNode);
+            replaceTextInInputValue(childNode);
+            if (typeof childNode.childNodes == 'object') {
+                replaceTextOnNodes(childNode.childNodes);
+            }
+        });
+    });
 }
 
 const observer = new MutationObserver(function(mutations) {
@@ -79,17 +82,13 @@ const observer = new MutationObserver(function(mutations) {
     mutations.forEach((mutation) => {
         switch(mutation.type) {
             case 'childList':
-              /* One or more children have been added to and/or removed
-                 from the tree; see mutation.addedNodes and
-                 mutation.removedNodes */
-                 //TODO: Change text on new child, on text and input value
+                replaceTextOnNodes(mutation.addedNodes);
                 break;
             case 'attributes':
-                debugger;
-                replaceTextInInputValue(mutation);
+                replaceTextInInputValue(mutation.target);
                 break;
             case 'characterData':
-                replaceTextInCharacterData(mutation);
+                replaceTextInCharacterData(mutation.target);
                 break;
           }
       });
@@ -98,4 +97,3 @@ const observer = new MutationObserver(function(mutations) {
 });
 
 startObserving(observer);
-
